2862c89907b9c303c7b250b82491dac105020056
wchErkhan <75239458+wchErkhan@users.noreply.github.com>
2021-02-18 00:55:56 +0600

    Add files via upload

---
+++ practice_1/practice_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace practice_1
+{
+    class Program
+    {
+        //Дз и практика
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Zhumankul Erkhan");
+            //2
+            int result = Sum(24, 36);
+            Console.WriteLine(result);
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
